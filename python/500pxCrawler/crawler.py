7e35955fffbd90c9656c0b502997355e13629bdf
Jiang Nan <jnriver1986@gmail.com>
2014-04-03 23:27:44 +0800

    Complete

--- python/500pxCrawler/crawler.py
+++ python/500pxCrawler/crawler.py
@@ -49,7 +49,7 @@ def scan_page(site_url, folder_name):
         pic_info["rating"] = pqr(pqr(pic).find("div.rating")).text().encode('utf-8')
 
         i += 1
-        worker = Thread(target=download_pic , args= (pic_id, pic_info, i, page_no, folder_name))
+        worker = Thread(target=download_pic , args=(pic_id, pic_info, i, page_no, folder_name))
         work_list.append(worker)
         worker.start()
     for work in work_list:
@@ -67,15 +67,24 @@ if __name__ == '__main__':
 
     query_word = "Sri+Lanka"
     from_idx = 1
-    to_idx = 3
+    to_idx = 1
+
+    page_no = from_idx - 1
 
     print "Create folder: " + query_word
     if not os.path.exists(query_word):
         os.mkdir(query_word)
 
+    page_work_list = []
+
     for page in xrange(from_idx, to_idx + 1):
         page_no += 1
         site_url = "http://500px.com/search?exclude_nude=true&type=photos&page=" + str(page) + "&q=" + query_word
         scan_page(site_url, query_word)
+    #     page_work = Thread(target=scan_page , args=(site_url, query_word))
+    #     page_work_list.append(page_work)
+    #     page_work.start()
+    # for page_work in page_work_list:
+    #     page_work.join()
 
     print "Complete!!!"
